76354dbb842604120373c42dcb648d28450342df
Kuldip4135 <thakkarkuldip29@gmail.com>
2020-09-25 17:16:42 +0530

    Updates

---
+++ precache-manifest.a7b32d5069c7cd85367d7f5d347a51b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56461c4ea6d0a1f1371b5703c5afe10",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "25e6b9bba5dcf91ed6cf",
+    "url": "/weather-app/static/css/main.4da3ca73.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e6b9bba5dcf91ed6cf",
+    "url": "/weather-app/static/js/main.2c436551.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
